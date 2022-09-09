ac309c7b0020268becf2158e5c544af1d37604ae
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-09-09 20:40:56 +0800

    學習紀錄

--- Program.cs
+++ Program.cs
@@ -10,10 +10,23 @@ namespace ConsoleApp03._01._04
     {
         static void Main(string[] args)
         {
+            int number1 = 10, number2 = 99;
+
+            // step 1,先用 temp變數存放 number1 值
+            int temp = number1;
+
+            // step 2, 異動 number1 值
+            number1 = number2;
+
+            // step 3, 異動 number2 值
+            number2 = temp;
+
+            Console.WriteLine($"對調的結果: number1={number1}, number2={number2}");
 
 
+            //應用時機:
 
-
+            //有時在做查詢功能時, 例如想找出某一日期區間的訂單, 但使用者居然吧開始 / 結束日的值寫相反, 或者想查詢售價在某二個數值之間的商品, 但把數值大小寫相反; 就會需要把值對調
         }
     }
 }
