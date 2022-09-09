3fbb519b0987f9a9cb29dc12ea50fe546af8be2b
TEDLIUCOMPUTER\asus <qsc811022@gmail.com>
2022-09-09 20:40:18 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp03._01._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+        }
+    }
+}
